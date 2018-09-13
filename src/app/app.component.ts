9add3a211f3651dc3bd83a7a58f5a86b60d76c68
core-isban-madrid <ximipedro73@gmail.com>
2018-09-13 14:30:31 +0200

    sc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'practica09';
+export class AppComponent implements OnInit {
+
+  ngOnInit(){
+    $(document).ready(function(){
+      // código Jquery
+    });
+  }
+
 }
---
+++ src/app/clientes/crear-cliente/crear-cliente.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-crear-cliente',
+  templateUrl: './crear-cliente.component.html',
+  styleUrls: ['./crear-cliente.component.css']
+})
+export class CrearClienteComponent implements OnInit {
+
+  cliente: object = {};
+
+  constructor() { }
+
+  ngOnInit() {
+    
+  }
+
+  crearCliente(formCliente){
+    console.log(formCliente)
+    this.cliente = {
+      nombre: formCliente.controls.nombre.value,
+      cif: formCliente.controls.cif.value,
+      email: formCliente.controls.email.value
+    }
+  }
+
+}
